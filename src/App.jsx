3d35f93c02c931f6f384f862473f63f184129920
Benasi000 <bendzo13@gmail.com>
2022-08-14 12:26:12 +0200

    Revert "MWA: [0007] adding style which are linked to a string"
    This reverts commit 224fa8c186136d028f5d63d9cf52cc8f80f22b2f.

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='bg-primary w-full overflow-hidden'>
-      <div>
-
-      </div>
-    </div>
+    <div>App</div>
   )
 }
 
